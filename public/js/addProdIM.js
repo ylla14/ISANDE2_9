d504eb6811838a8af2e3cdac3e29cc4367990dce
Allyssa Salgado <allyssa_salgado@dlsu.edu.ph>
2024-11-13 17:11:01 +0800

    prod deets

---
+++ public/js/addProdIM.js
@@ -0,0 +1,22 @@
+document.getElementById("home-link").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default action of the link
+    window.location.href = "dashboardIM.html"; // Redirect to the inventory page
+});
+
+// Select the Inventory link by its ID
+document.getElementById("inventory-link").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default action of the link
+    window.location.href = "inventoryIM.html"; // Redirect to the inventory page
+});
+
+document.getElementById("sales-link").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default action of the link
+    window.location.href = "salesOrderIM.html"; // Redirect to the inventory page
+});
+
+
+// Select the Inventory link by its ID
+document.getElementById("suppliers-link").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default action of the link
+    window.location.href = "suppliersIM.html"; // Redirect to the inventory page
+});
